6f617d16aae793e19a69f0260446a770529347dc
zotherstupidguy <zotherstupidguy@gmail.com>
2015-06-08 09:42:45 +0200

    project contribute

---
+++ lib/find_contr.rb
@@ -0,0 +1,6 @@
+# this helps you find contributors for your opensource project
+# Usage from IRC via sending a msg
+# zotherstupidbot wanted for myprojecturl
+# the bot checks the url for a Gemfile, reads the names of the gems as IRC channels and then return to you top 10 irc users who join those most of those channels
+# and sends them a msg that you want them to join your project and contribute.
+#
